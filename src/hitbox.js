df984e88d1cfe5994a19e3c78b0a9fd35a28d5d2
Marco Santarelli <marco@backbase.com>
2020-11-18 09:44:48 +0100

    Avoid using deep imports

--- src/hitbox.js
+++ src/hitbox.js
@@ -1,4 +1,6 @@
-import {merge} from 'chart.js/helpers';
+import Chart from 'chart.js';
+
+var helpers = Chart.helpers;
 
 var MIN_INTEGER = Number.MIN_SAFE_INTEGER || -9007199254740991; // eslint-disable-line es/no-number-minsafeinteger
 var MAX_INTEGER = Number.MAX_SAFE_INTEGER || 9007199254740991;  // eslint-disable-line es/no-number-maxsafeinteger
@@ -59,7 +61,7 @@ var HitBox = function() {
 	};
 };
 
-merge(HitBox.prototype, {
+helpers.merge(HitBox.prototype, {
 	center: function() {
 		var r = this._rect;
 		return {
