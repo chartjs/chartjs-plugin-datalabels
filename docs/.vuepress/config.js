55fdaa83a29039b2865ef5c0eb850c8815dc1049
Simon Brunel <simonbrunel@users.noreply.github.com>
2021-04-12 22:54:53 +0200

    Add vuepress-plugin-versions to the docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,9 @@
+// https://docs.netlify.com/configure-builds/environment-variables/#git-metadata
+const BRANCH = process.env.BRANCH || (process.env.NODE_ENV === 'development' ? 'local' : '');
+const IS_DEV = BRANCH ? !BRANCH.match(/^v\d\.\d\.\d/) : false;
+const REPO_NAME = 'chartjs/chartjs-plugin-datalabels';
+const REPO_URL = `https://www.github.com/${REPO_NAME}`;
+
 module.exports = {
   dest: 'dist/docs',
   theme: 'chartjs',
@@ -16,9 +22,53 @@ module.exports = {
         {base: '/samples', alternative: ['charts/line']},
       ],
     }],
+    ['@simonbrunel/vuepress-plugin-versions', {
+      filters: {
+        compat: (major) => +major < 2 ? 2 : 3,
+        slug: (v) => v.replace(/\./g, '_'),
+        suffix: (v) => v ? ` (${v})` : '',
+      },
+      menu: {
+        text: IS_DEV ? `Development (${BRANCH})` : '{{version}}',
+        items: [
+          {
+            text: 'Documentation',
+            items: [
+              {
+                target: '_self',
+                text: 'Development (master)',
+                link: 'https://master--chartjs-plugin-datalabels.netlify.app/',
+              },
+              {
+                type: 'versions',
+                text: '{{major}}.{{minor}}.x - Chart.js v{{major|compat}}',
+                link: 'https://v{{version|slug}}--chartjs-plugin-datalabels.netlify.app/',
+                target: '_self',
+                group: 'major',
+              },
+            ],
+          },
+          {
+            text: 'Release notes',
+            items: [
+              {
+                type: 'versions',
+                text: 'v{{version}}{{tag|suffix}}',
+                link: `${REPO_URL}/releases/tag/v{{version}}`,
+                group: 'major',
+              },
+              {
+                text: 'All releases',
+                link: `${REPO_URL}/releases`,
+              },
+            ],
+          },
+        ],
+      }
+    }]
   ],
   themeConfig: {
-    repo: 'chartjs/chartjs-plugin-datalabels',
+    repo: REPO_NAME,
     docsDir: 'docs',
     editLinks: true,
     logo: '/favicon.png',
